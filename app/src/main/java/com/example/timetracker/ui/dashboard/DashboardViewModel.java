00010531bd6d08da9d97875f79e11997cdceab71
nico_devanthery <devantherynicolas@gmail.com>
2022-02-23 22:53:30 +0100

    first commit

---
+++ app/src/main/java/com/example/timetracker/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.timetracker.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the add menu");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
